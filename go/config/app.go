114b472d201a295d1c9f107a8aae9be50fa57cfe
Zaaksam <zaaksam@qq.com>
2017-08-08 15:52:43 +0800

    修正参数不起作用的bug，增加ui参数

--- go/config/app.go
+++ go/config/app.go
@@ -1,6 +1,9 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"time"
+)
 
 // AppConf 应用全局参数
 var AppConf appConf
@@ -10,6 +13,8 @@ func init() {
 	flag.BoolVar(&AppConf.AutoOpen, "autoopen", true, "自动打开浏览器，默认：true")
 	flag.StringVar(&AppConf.IP, "ip", "", "监听的IP地址，默认：127.0.0.1")
 	flag.IntVar(&AppConf.Port, "port", 0, "服务端口，默认：8080")
+	flag.BoolVar(&AppConf.UI, "ui", true, "是否开启WebUI管理服务，默认：true")
+	flag.Parse()
 
 	// authURL?redirect_uri=http://127.0.0.1:8080/web/auth&code=abc
 	// redirect时，须在uri上附上 code、userID、userName 参数
@@ -22,11 +27,15 @@ func init() {
 	if AppConf.Port <= 0 {
 		AppConf.Port = 8080
 	}
+
+	AppConf.Started = time.Now().Unix()
 }
 
 type appConf struct {
 	Debug    bool
 	AutoOpen bool
+	UI       bool
 	IP       string
 	Port     int
+	Started  int64
 }
