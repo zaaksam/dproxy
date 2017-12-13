3d135bc4dcd91b404d9bda737c7c177fdd76d99c
Zaaksam <zaaksam@qq.com>
2017-12-13 18:02:39 +0800

    更新Vue新版声明定义

--- web/vue/log.vue
+++ web/vue/log.vue
@@ -24,107 +24,117 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
-import _ from 'lodash'
-import moment from 'moment'
-import MyPage from './page.vue'
-import API from '../ts/api'
+import { CreateElement } from "vue";
+import { Vue, Component } from "vue-property-decorator";
+import _ from "lodash";
+import moment from "moment";
+import MyPage from "./page.vue";
+import API from "../ts/api";
 
 @Component({
-    components: {
-        MyPage
-    }
+  components: {
+    MyPage
+  }
 })
 export default class MyLog extends Vue {
-    query: Model.Log = {
-        type: 'All',
-        content: '',
-        created: 0
+  query: Model.Log = {
+    type: "All",
+    content: "",
+    created: 0
+  };
+
+  tableData: APIListModel<Model.Log> = {
+    pageIndex: 1,
+    pageSize: 10,
+    pageCount: 0,
+    total: 0,
+    items: []
+  };
+
+  tableColumns: any[] = <any>[
+    {
+      title: "类型",
+      key: "type"
+    },
+    {
+      title: "内容",
+      key: "content"
+    },
+    {
+      title: "记录时间",
+      key: "created",
+      render: (h: CreateElement, params: any) => {
+        return h(
+          "span",
+          moment.unix(params.row.created).format("YYYY-MM-DD HH:mm:ss")
+        );
+      }
+    }
+  ];
+
+  mounted() {
+    this.onLoad();
+  }
+
+  async onLoad() {
+    let pi = _.parseInt(this.$route.query.pi);
+    let ps = _.parseInt(this.$route.query.ps);
+    if (_.isNaN(pi)) {
+      pi = this.tableData.pageIndex;
+    }
+    if (_.isNaN(ps)) {
+      ps = this.tableData.pageSize;
     }
 
-    tableData: APIListModel<Model.Log> = {
-        pageIndex: 1,
-        pageSize: 10,
-        pageCount: 0,
-        total: 0,
-        items: []
+    let pms = new URLSearchParams();
+    pms.set("pageIndex", pi.toString());
+    pms.set("pageSize", ps.toString());
+
+    let result = await API.get<Model.Logs>("/log/list/?" + pms.toString());
+    if (result.code === 10000) {
+      this.tableData = result.data!.list;
+    } else {
+      this.$Message.error({
+        duration: 5,
+        content: result.msg + "(" + result.code.toString() + ")"
+      });
+    }
+  }
+
+  async onDel() {
+    if (this.query.created <= 0) {
+      this.$Message.error({ duration: 5, content: "指定时间必须选择" });
+      return;
     }
 
-    tableColumns: any[] = <any>[
-        {
-            title: '类型',
-            key: 'type'
-        },
-        {
-            title: '内容',
-            key: 'content'
-        },
-        {
-            title: '记录时间',
-            key: 'created',
-            render: (h: Vue.CreateElement, params: any) => {
-                return h('span', moment.unix(params.row.created).format('YYYY-MM-DD HH:mm:ss'))
-            }
-        }
-    ]
+    let pms = new URLSearchParams();
+    pms.set("created", this.query.created.toString());
 
-    mounted() {
-        this.onLoad()
+    if (this.query.type != "All") {
+      pms.set("type", this.query.type);
+    }
+    if (this.query.content != "") {
+      pms.set("content", this.query.content);
     }
 
-    async onLoad() {
-        let pi = _.parseInt(this.$route.query.pi)
-        let ps = _.parseInt(this.$route.query.ps)
-        if (_.isNaN(pi)) {
-            pi = this.tableData.pageIndex
-        }
-        if (_.isNaN(ps)) {
-            ps = this.tableData.pageSize
-        }
-
-        let pms = new URLSearchParams()
-        pms.set('pageIndex', pi.toString())
-        pms.set('pageSize', ps.toString())
-
-        let result = await API.get<Model.Logs>('/log/list/?' + pms.toString())
-        if (result.code === 10000) {
-            this.tableData = result.data!.list
-        } else {
-            this.$Message.error({ duration: 5, content: result.msg + '(' + result.code.toString() + ')' })
-        }
+    let result = await API.delete<any>("/log/?" + pms.toString());
+    if (result.code === 10000) {
+      this.onLoad();
+    } else {
+      this.$Message.error({
+        duration: 5,
+        content: result.msg + "(" + result.code.toString() + ")"
+      });
     }
+  }
 
-    async onDel() {
-        if (this.query.created <= 0) {
-            this.$Message.error({ duration: 5, content: '指定时间必须选择' })
-            return
-        }
-
-        let pms = new URLSearchParams()
-        pms.set('created', this.query.created.toString())
-
-        if (this.query.type != 'All') {
-            pms.set('type', this.query.type)
-        }
-        if (this.query.content != '') {
-            pms.set('content', this.query.content)
-        }
-
-        let result = await API.delete<any>('/log/?' + pms.toString())
-        if (result.code === 10000) {
-            this.onLoad()
-        } else {
-            this.$Message.error({ duration: 5, content: result.msg + '(' + result.code.toString() + ')' })
-        }
+  onDatetimeChange(expiredStr: string) {
+    let unix = moment(expiredStr, "YYYY-MM-DD HH:mm:ss").unix();
+    if (_.isNaN(unix)) {
+      this.query.created = 0;
+    } else {
+      this.query.created = unix;
     }
-
-    onDatetimeChange(expiredStr: string) {
-        let unix = moment(expiredStr, 'YYYY-MM-DD HH:mm:ss').unix()
-        if (_.isNaN(unix)) {
-            this.query.created = 0
-        } else {
-            this.query.created = unix
-        }
-    }
+  }
 }
 </script>
--- web/vue/whiteList.vue
+++ web/vue/whiteList.vue
@@ -32,284 +32,310 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
-import _ from 'lodash'
-import moment from 'moment'
-import MyPage from './page.vue'
-import API from '../ts/api'
+import { CreateElement } from "vue";
+import { Vue, Component } from "vue-property-decorator";
+import _ from "lodash";
+import moment from "moment";
+import MyPage from "./page.vue";
+import API from "../ts/api";
 
 interface modalDataModel {
-    id: number
-    ip: string
-    userID: string
-    userName: string
-    expired: number
+  id: number;
+  ip: string;
+  userID: string;
+  userName: string;
+  expired: number;
 }
 
 interface modalModel extends BaseModalModel {
-    expiredStr: string
-    data: modalDataModel
+  expiredStr: string;
+  data: modalDataModel;
 }
 
 @Component({
-    components: {
-        MyPage
-    }
+  components: {
+    MyPage
+  }
 })
 export default class MyWhiteList extends Vue {
-    modal: modalModel = {
-        isShow: false,
-        isLoading: false,
-        isErr: false,
-        errMsg: '',
-        isEdit: false,
-        title: '',
-        okBtnText: '',
-        expiredStr: '',
-        data: {
-            id: 0,
-            ip: '',
-            userID: '',
-            userName: '',
-            expired: 0
-        }
+  modal: modalModel = {
+    isShow: false,
+    isLoading: false,
+    isErr: false,
+    errMsg: "",
+    isEdit: false,
+    title: "",
+    okBtnText: "",
+    expiredStr: "",
+    data: {
+      id: 0,
+      ip: "",
+      userID: "",
+      userName: "",
+      expired: 0
     }
-    dateOptions: any = {
-        disabledDate: (date: Date) => {
-            // - 86400000
-            return date && date.valueOf() < Date.now()
-        }
+  };
+  dateOptions: any = {
+    disabledDate: (date: Date) => {
+      // - 86400000
+      return date && date.valueOf() < Date.now();
     }
+  };
 
-    tableData: APIListModel<Model.WhiteList> = {
-        pageIndex: 1,
-        pageSize: 10,
-        pageCount: 0,
-        total: 0,
-        items: []
-    }
+  tableData: APIListModel<Model.WhiteList> = {
+    pageIndex: 1,
+    pageSize: 10,
+    pageCount: 0,
+    total: 0,
+    items: []
+  };
 
-    tableColumns: any[] = [
-        {
-            title: 'ID',
-            key: 'id'
-        },
-        {
-            title: 'IP',
-            key: 'ip'
-        },
-        {
-            title: '申请人ID',
-            key: 'userID'
-        },
-        {
-            title: '申请人名称',
-            key: 'userName'
-        },
-        {
-            title: '申请时间',
-            key: 'created',
-            render: (h: Vue.CreateElement, params: any) => {
-                return h('span', moment.unix(params.row.created).format('YYYY-MM-DD HH:mm:ss'))
+  tableColumns: any[] = [
+    {
+      title: "ID",
+      key: "id"
+    },
+    {
+      title: "IP",
+      key: "ip"
+    },
+    {
+      title: "申请人ID",
+      key: "userID"
+    },
+    {
+      title: "申请人名称",
+      key: "userName"
+    },
+    {
+      title: "申请时间",
+      key: "created",
+      render: (h: CreateElement, params: any) => {
+        return h(
+          "span",
+          moment.unix(params.row.created).format("YYYY-MM-DD HH:mm:ss")
+        );
+      }
+    },
+    {
+      title: "过期时间",
+      key: "expired",
+      render: (h: CreateElement, params: any) => {
+        return h(
+          "span",
+          moment.unix(params.row.expired).format("YYYY-MM-DD HH:mm:ss")
+        );
+      }
+    },
+    {
+      width: 160,
+      renderHeader: (h: CreateElement, params: any) => {
+        return h("Button", {
+          props: {
+            size: "small",
+            type: "primary",
+            icon: "plus-round"
+          },
+          on: {
+            click: () => {
+              this.onModalShow();
             }
-        },
-        {
-            title: '过期时间',
-            key: 'expired',
-            render: (h: Vue.CreateElement, params: any) => {
-                return h('span', moment.unix(params.row.expired).format('YYYY-MM-DD HH:mm:ss'))
-            }
-        },
-        {
-            width: 160,
-            renderHeader: (h: Vue.CreateElement, params: any) => {
-                return h('Button', {
-                    props: {
-                        size: 'small',
-                        type: 'primary',
-                        icon: 'plus-round'
-                    },
-                    on: {
-                        click: () => {
-                            this.onModalShow()
-                        }
-                    }
-                })
+          }
+        });
+      },
+      render: (h: CreateElement, params: any) => {
+        return h("div", [
+          h("Button", {
+            props: {
+              size: "small",
+              icon: "edit",
+              type: "info"
             },
-            render: (h: Vue.CreateElement, params: any) => {
-                return h('div', [
-                    h('Button', {
-                        props: {
-                            size: 'small',
-                            icon: 'edit',
-                            type: 'info'
-                        },
-                        style: {
-                            marginRight: '10px'
-                        },
-                        on: {
-                            click: () => {
-                                this.onEdit(params.index)
-                            }
-                        }
-                    }),
-                    h('Poptip', {
-                        props: {
-                            confirm: true,
-                            placement: 'left',
-                            title: '您确认要删除这条内容吗？'
-                        },
-                        on: {
-                            'on-ok': () => {
-                                this.onDel(params.index)
-                            }
-                        }
-                    }, [
-                            h('Button', {
-                                props: {
-                                    size: 'small',
-                                    icon: 'close-round',
-                                    type: 'error'
-                                }
-                            })
-                        ])
-
-                ])
+            style: {
+              marginRight: "10px"
+            },
+            on: {
+              click: () => {
+                this.onEdit(params.index);
+              }
             }
-        }
-    ]
+          }),
+          h(
+            "Poptip",
+            {
+              props: {
+                confirm: true,
+                placement: "left",
+                title: "您确认要删除这条内容吗？"
+              },
+              on: {
+                "on-ok": () => {
+                  this.onDel(params.index);
+                }
+              }
+            },
+            [
+              h("Button", {
+                props: {
+                  size: "small",
+                  icon: "close-round",
+                  type: "error"
+                }
+              })
+            ]
+          )
+        ]);
+      }
+    }
+  ];
 
-    mounted() {
-        this.onLoad()
+  mounted() {
+    this.onLoad();
+  }
+
+  async onLoad() {
+    let pi = _.parseInt(this.$route.query.pi);
+    let ps = _.parseInt(this.$route.query.ps);
+    if (_.isNaN(pi)) {
+      pi = this.tableData!.pageIndex;
+    }
+    if (_.isNaN(ps)) {
+      ps = this.tableData!.pageSize;
     }
 
-    async onLoad() {
-        let pi = _.parseInt(this.$route.query.pi)
-        let ps = _.parseInt(this.$route.query.ps)
-        if (_.isNaN(pi)) {
-            pi = this.tableData!.pageIndex
-        }
-        if (_.isNaN(ps)) {
-            ps = this.tableData!.pageSize
-        }
+    let pms = new URLSearchParams();
+    pms.append("pageIndex", pi.toString());
+    pms.append("pageSize", ps.toString());
 
-        let pms = new URLSearchParams()
-        pms.append('pageIndex', pi.toString())
-        pms.append('pageSize', ps.toString())
+    let result = await API.get<Model.WhiteLists>(
+      "/whitelist/list/?" + pms.toString()
+    );
+    if (result.code === 10000) {
+      this.tableData = result.data!.list;
+    } else {
+      this.$Message.error({
+        duration: 5,
+        content: result.msg + "(" + result.code.toString() + ")"
+      });
+    }
+  }
 
-        let result = await API.get<Model.WhiteLists>('/whitelist/list/?' + pms.toString())
-        if (result.code === 10000) {
-            this.tableData = result.data!.list
-        } else {
-            this.$Message.error({ duration: 5, content: result.msg + '(' + result.code.toString() + ')' })
-        }
+  onEdit(index: number) {
+    let data = this.tableData!.items[index];
+    if (data) {
+      this.onModalShow(data);
+    }
+  }
+
+  async onDel(index: number) {
+    let id: number = this.tableData!.items[index].id!;
+    if (!id) {
+      return;
     }
 
-    onEdit(index: number) {
-        let data = this.tableData!.items[index]
-        if (data) {
-            this.onModalShow(data)
-        }
+    let result = await API.delete<any>("/whitelist/" + id.toString());
+    if (result.code === 10000) {
+      this.onLoad();
+    } else {
+      this.$Message.error({
+        duration: 5,
+        content: result.msg + "(" + result.code.toString() + ")"
+      });
+    }
+  }
+
+  onModalShow(data?: Model.WhiteList) {
+    this.modal.isShow = true;
+    this.modal.isLoading = false;
+    this.modal.isErr = false;
+    this.modal.errMsg = "";
+
+    if (data) {
+      this.modal.isEdit = true;
+      this.modal.title = "白名单修改";
+      this.modal.okBtnText = "修改";
+
+      this.modal.data = <modalDataModel>_.pick(data, _.keys(this.modal.data));
+      this.modal.expiredStr = moment
+        .unix(this.modal.data.expired)
+        .format("YYYY-MM-DD HH:mm:ss");
+    } else {
+      this.modal.isEdit = false;
+      this.modal.title = "白名单申请";
+      this.modal.okBtnText = "申请";
+
+      this.modal.data = {
+        id: 0,
+        ip: "",
+        userID: "",
+        userName: "",
+        expired: 0
+      };
+      // this.modal.expiredDate = moment.unix(this.modal.data.expired).toDate()
+      this.modal.expiredStr = "";
+    }
+  }
+
+  onModalDatetimeChange(expiredStr: string) {
+    this.modal.expiredStr = expiredStr;
+
+    let unix = moment(expiredStr, "YYYY-MM-DD HH:mm:ss").unix();
+    if (_.isNaN(unix)) {
+      this.modal.data.expired = 0;
+    } else {
+      this.modal.data.expired = unix;
+    }
+  }
+
+  async onModalOk() {
+    this.modal.isErr = false;
+    this.modal.errMsg = "";
+
+    let errMsg: string = "";
+    if (this.modal.data.ip == "") {
+      errMsg = "IP不能为空";
+    } else if (this.modal.data.userID == "") {
+      errMsg = "申请者ID不能为空";
+    } else if (this.modal.data.userName == "") {
+      errMsg = "申请者名称不能为空";
+    }
+    if (errMsg != "") {
+      this.modal.errMsg = errMsg;
+      this.modal.isErr = true;
+      return;
     }
 
-    async onDel(index: number) {
-        let id: number = this.tableData!.items[index].id!
-        if (!id) {
-            return
-        }
-
-        let result = await API.delete<any>('/whitelist/' + id.toString())
-        if (result.code === 10000) {
-            this.onLoad()
-        } else {
-            this.$Message.error({ duration: 5, content: result.msg + '(' + result.code.toString() + ')' })
-        }
+    this.modal.isLoading = true;
+    let id: string = "";
+    if (this.modal.data.id > 0) {
+      id = "/" + this.modal.data.id.toString();
     }
 
-    onModalShow(data?: Model.WhiteList) {
-        this.modal.isShow = true
-        this.modal.isLoading = false
-        this.modal.isErr = false
-        this.modal.errMsg = ''
-
-        if (data) {
-            this.modal.isEdit = true
-            this.modal.title = '白名单修改'
-            this.modal.okBtnText = '修改'
-
-            this.modal.data = _.pick(data, _.keys(this.modal.data))
-            this.modal.expiredStr = moment.unix(this.modal.data.expired).format('YYYY-MM-DD HH:mm:ss')
-        } else {
-            this.modal.isEdit = false
-            this.modal.title = '白名单申请'
-            this.modal.okBtnText = '申请'
-
-            this.modal.data = {
-                id: 0,
-                ip: '',
-                userID: '',
-                userName: '',
-                expired: 0
-            }
-            // this.modal.expiredDate = moment.unix(this.modal.data.expired).toDate()
-            this.modal.expiredStr = ''
-        }
+    let result = await API.put<Model.WhiteListAlias>(
+      "/whitelist" + id,
+      this.modal.data
+    );
+    if (result.code === 10000) {
+      if (this.modal.data.id > 0) {
+        this.onLoad();
+      } else {
+        this.tableData!.items.unshift(result.data!.whiteList);
+        this.tableData!.total += 1;
+      }
+      this.modal.isShow = false;
+    } else {
+      this.$Message.error({
+        duration: 5,
+        content: result.msg + "(" + result.code.toString() + ")"
+      });
+      this.modal.errMsg = result.msg + "(" + result.code.toString() + ")";
+      this.modal.isErr = true;
     }
+    this.modal.isLoading = false;
+  }
 
-    onModalDatetimeChange(expiredStr: string) {
-        this.modal.expiredStr = expiredStr
-
-        let unix = moment(expiredStr, 'YYYY-MM-DD HH:mm:ss').unix()
-        if (_.isNaN(unix)) {
-            this.modal.data.expired = 0
-        } else {
-            this.modal.data.expired = unix
-        }
-    }
-
-    async onModalOk() {
-        this.modal.isErr = false
-        this.modal.errMsg = ''
-
-        let errMsg: string = ''
-        if (this.modal.data.ip == '') {
-            errMsg = 'IP不能为空'
-        } else if (this.modal.data.userID == '') {
-            errMsg = '申请者ID不能为空'
-        } else if (this.modal.data.userName == '') {
-            errMsg = '申请者名称不能为空'
-        }
-        if (errMsg != '') {
-            this.modal.errMsg = errMsg
-            this.modal.isErr = true
-            return
-        }
-
-        this.modal.isLoading = true
-        let id: string = ''
-        if (this.modal.data.id > 0) {
-            id = '/' + this.modal.data.id.toString()
-        }
-
-        let result = await API.put<Model.WhiteListAlias>('/whitelist' + id, this.modal.data)
-        if (result.code === 10000) {
-            if (this.modal.data.id > 0) {
-                this.onLoad()
-            } else {
-                this.tableData!.items.unshift(result.data!.whiteList)
-                this.tableData!.total += 1
-            }
-            this.modal.isShow = false
-        } else {
-            this.$Message.error({ duration: 5, content: result.msg + '(' + result.code.toString() + ')' })
-            this.modal.errMsg = result.msg + '(' + result.code.toString() + ')'
-            this.modal.isErr = true
-        }
-        this.modal.isLoading = false
-    }
-
-    onModalCancel() {
-        this.modal.isShow = false
-    }
+  onModalCancel() {
+    this.modal.isShow = false;
+  }
 }
 </script>
