7c3ba8c6fce0d6ac5f8cdcc82d4f47bc02588386
Zaaksam <zaaksam@qq.com>
2021-08-27 23:00:13 +0800

    chore: 更新 API 接口

--- web/vue/doc.vue
+++ web/vue/doc.vue
@@ -1,54 +1,55 @@
 <template>
     <Row>
         <Col span="5">
-        <Menu theme="light" width="auto" @on-select="onSelect">
-            <Submenu name="proxy">
-                <template slot="title">
-                    <Icon type="ios-world-outline"></Icon>
-                    请求代理
-                </template>
-                <Menu-item name="proxy.start">启动</Menu-item>
-                <Menu-item name="proxy.stop">停止</Menu-item>
-            </Submenu>
-            <Submenu name="whiteList">
-                <template slot="title">
-                    <Icon type="ios-navigate"></Icon>
-                    白名单
-                </template>
-                <Menu-item name="whiteList.get">获取</Menu-item>
-                <Menu-item name="whiteList.list">列表</Menu-item>
-                <Menu-item name="whiteList.add">添加</Menu-item>
-                <Menu-item name="whiteList.update">修改</Menu-item>
-                <Menu-item name="whiteList.delete">删除</Menu-item>
-            </Submenu>
-            <Submenu name="portMap">
-                <template slot="title">
-                    <Icon type="arrow-swap"></Icon>
-                    端口映射
-                </template>
-                <Menu-item name="portMap.get">获取</Menu-item>
-                <Menu-item name="portMap.list">列表</Menu-item>
-                <Menu-item name="portMap.add">添加</Menu-item>
-                <Menu-item name="portMap.update">修改</Menu-item>
-                <Menu-item name="portMap.delete">删除</Menu-item>
-            </Submenu>
-            <Submenu name="log">
-                <template slot="title">
-                    <Icon type="ios-analytics"></Icon>
-                    日志
-                </template>
-                <Menu-item name="log.list">列表</Menu-item>
-            </Submenu>
-        </Menu>
+            <Menu theme="light" width="auto" @on-select="onSelect">
+                <Submenu name="proxy">
+                    <template slot="title">
+                        <Icon type="ios-world-outline"></Icon>
+                        请求代理
+                    </template>
+                    <Menu-item name="proxy.start">启动</Menu-item>
+                    <Menu-item name="proxy.stop">停止</Menu-item>
+                </Submenu>
+                <Submenu name="whiteList">
+                    <template slot="title">
+                        <Icon type="ios-navigate"></Icon>
+                        白名单
+                    </template>
+                    <Menu-item name="whiteList.get">获取</Menu-item>
+                    <Menu-item name="whiteList.list">列表</Menu-item>
+                    <Menu-item name="whiteList.clear">清理过期</Menu-item>
+                    <Menu-item name="whiteList.add">添加</Menu-item>
+                    <Menu-item name="whiteList.update">修改</Menu-item>
+                    <Menu-item name="whiteList.delete">删除</Menu-item>
+                </Submenu>
+                <Submenu name="portMap">
+                    <template slot="title">
+                        <Icon type="arrow-swap"></Icon>
+                        端口映射
+                    </template>
+                    <Menu-item name="portMap.get">获取</Menu-item>
+                    <Menu-item name="portMap.list">列表</Menu-item>
+                    <Menu-item name="portMap.add">添加</Menu-item>
+                    <Menu-item name="portMap.update">修改</Menu-item>
+                    <Menu-item name="portMap.delete">删除</Menu-item>
+                </Submenu>
+                <Submenu name="log">
+                    <template slot="title">
+                        <Icon type="ios-analytics"></Icon>
+                        日志
+                    </template>
+                    <Menu-item name="log.list">列表</Menu-item>
+                </Submenu>
+            </Menu>
         </Col>
         <Col push="2" span="17">
-        <div>
-            <Form>
-                <Form-item label="地址：">{{doc.path}}</Form-item>
-                <Form-item label="方法：">{{doc.method}}</Form-item>
-                <Form-item label="说明：">{{doc.title}}</Form-item>
-            </Form>
-        </div>
+            <div>
+                <Form>
+                    <Form-item label="地址：">{{ doc.path }}</Form-item>
+                    <Form-item label="方法：">{{ doc.method }}</Form-item>
+                    <Form-item label="说明：">{{ doc.title }}</Form-item>
+                </Form>
+            </div>
         </Col>
     </Row>
 </template>
@@ -95,6 +96,11 @@ export default class Doc extends Vue {
             title: '获取白名单列表',
             method: 'GET',
             path: '/api/whitelist/list'
+        }, {
+            alias: 'whiteList.clear',
+            title: '清理过过期白名单',
+            method: 'DELETE',
+            path: '/api/whitelist/clear'
         }, {
             alias: 'whiteList.add',
             title: '添加白名单',
